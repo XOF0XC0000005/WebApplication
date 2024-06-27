1affad7208894bc8e3fd2cd402e671241227d1b8
XOF0XC0000005 <leva95reva@yahoo.com>
2024-06-27 16:44:49 +0300

    Initial Commit

---
+++ MarketProject/Abstractions/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Market.Models.DTO;
+
+namespace Market.Abstractions
+{
+    public interface IProductRepository
+    {
+        public int AddGroup(ProductGroupDto group);
+        public IEnumerable<ProductGroupDto> GetGroups();
+        public int AddProduct(ProductDto product);
+        public IEnumerable<ProductDto> GetProducts();
+        public string GetProductsCsv();
+        public string GetStatisticFile();
+    }
+}

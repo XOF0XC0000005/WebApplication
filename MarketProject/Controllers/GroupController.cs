cc1d913d71e36f731753fd852992647f8b7e0ab0
XOF0XC0000005 <leva95reva@yahoo.com>
2024-06-27 16:45:36 +0300

    ControllersCommit

---
+++ MarketProject/Controllers/GroupController.cs
@@ -0,0 +1,59 @@
+﻿using Market.Abstractions;
+using Market.Models.DTO;
+using Market.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Market.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+
+        public GroupController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpDelete("delete_group/{id}")]
+        public IActionResult DeleteGroup(int id)
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    if (context.ProductGroups.Any(x => x.Id == id))
+                    {
+                        context.Remove(id);
+                        context.SaveChanges();
+                        return Ok();
+                    }
+                    else
+                    {
+                        return StatusCode(409);
+                    }
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPost("add_group")]
+        public IActionResult AddGroup([FromBody] ProductGroupDto productGroupDto)
+        {
+            var result = _repository.AddGroup(productGroupDto);
+            return Ok(result);
+        }
+
+        [HttpGet("get_groups")]
+        public IActionResult GetGroups()
+        {
+            var groups = _repository.GetGroups();
+            return Ok(groups);
+        }
+    }
+}
---
+++ MarketProject/Controllers/ProductController.cs
@@ -0,0 +1,98 @@
+﻿using Market.Abstractions;
+using Market.Models.DTO;
+using Market.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Market.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("get_cache_statistic")]
+        public ActionResult<string> GetCacheStatistic() 
+        {
+            var fileName = _repository.GetStatisticFile();
+            return "https://" + Request.Host.ToString() + "/static/" + fileName;
+        }
+
+        [HttpGet("get_products")]
+        public IActionResult GetProducts()
+        {
+            var products = _repository.GetProducts();
+            return Ok(products);
+        }
+
+        [HttpGet(template: "get_products_csv")]
+        public FileContentResult GetProductsCsv() 
+        {
+            var result = _repository.GetProductsCsv();
+            return File(new System.Text.UTF8Encoding().GetBytes(result), "text/csv", "products_report.csv");
+        }
+
+        [HttpPost("add_products")]
+        public IActionResult AddProduct([FromBody] ProductDto productDto)
+        {
+            var result = _repository.AddProduct(productDto);
+            return Ok(result);
+        }
+
+        [HttpPatch("patch_cost_product/{id}")]
+        public IActionResult PatchCostProduct([FromQuery] int id, int newCost)
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    if (context.Products.Any(x => x.Id == id))
+                    {
+                        var product = context.Products.FirstOrDefault(x => x.Id == id);
+                        product.Cost = newCost;
+                        context.SaveChanges();
+                        return Ok();
+                    }
+                    else
+                    {
+                        return StatusCode(404);
+                    }
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpDelete("delete_product/{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    if (context.Products.Any(x => x.Id == id))
+                    {
+                        context.Remove(id);
+                        context.SaveChanges();
+                        return Ok();
+                    }
+                    else
+                    {
+                        return StatusCode(409);
+                    }
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}

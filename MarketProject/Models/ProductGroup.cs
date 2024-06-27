42c738f35f973a597c80e25bcbd8de80894863f4
XOF0XC0000005 <leva95reva@yahoo.com>
2024-06-27 16:42:29 +0300

    Delete MarketProject/Models directory

--- MarketProject/Models/ProductGroup.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Market.Models
-{
-    public class ProductGroup : BaseModel
-    {
-        public virtual List<Product> Products { get; set; } = new List<Product>();
-    }
-}
--- MarketProject/Models/Storage.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Market.Models
-{
-    public class Storage : BaseModel
-    {
-        public virtual List<Product> Products { get; set; } = new List<Product>();
-        public int Count { get; set; }
-    }
-}
